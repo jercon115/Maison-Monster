dc195b63c78aa6b882d12531e1006d5ca3503a2a
Jeremi Concepcion <jeremi.concepcion@gmail.com>
2015-02-01 19:37:22 -0600

    first commit

---
+++ Assets/Floors/Floor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+	
+	private Sprites[] cells;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
