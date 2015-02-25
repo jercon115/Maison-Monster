ba35619dfa83fd75d1a992ea783e140455f2bd37
JustinDC <JustinConcepcion2015@u.northwestern.edu>
2015-02-24 19:45:42 -0600

    Added zooming and new game font

--- Assets/Managers/GameCamera.cs
+++ Assets/Managers/GameCamera.cs
@@ -2,7 +2,11 @@
 using System.Collections;
 
 public class GameCamera : MonoBehaviour {
+
+	bool minusDown = false, equalsDown = false;
+
 	void Update () {
+		// Camera movement
 		int vert = 0, horiz = 0;
 		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow))
 			vert++;
@@ -14,6 +18,25 @@ public class GameCamera : MonoBehaviour {
 			horiz--;
 		transform.Translate (new Vector3(horiz*0.1f,vert*0.1f));
 
+		// Camera zooming
 
+		if (Input.GetKey (KeyCode.Minus)) {
+		    if (!minusDown)  {
+				minusDown = true; camera.orthographicSize++;
+			}
+		} else {
+			minusDown = false;
+		}
+		if (Input.GetKey (KeyCode.Equals)) {
+			if (!equalsDown)  {
+				equalsDown = true; camera.orthographicSize--;
+			}
+		} else {
+			equalsDown = false;
+		}
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0)
+		    camera.orthographicSize++;
+		if (Input.GetAxis("Mouse ScrollWheel") > 0)
+		    camera.orthographicSize--;
 	}
 }
